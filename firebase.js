2de005c3e3bb4563e61ffe71c700ef68ee846ee3
Tim Grootscholten <timgrootscholtentim@gmail.com>
2021-04-04 16:49:22 +0200

    commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDv_iC2-2GRwFDAOLkrIsOea7IMCTexHc8",
+  authDomain: "tournaments-c1329.firebaseapp.com",
+  projectId: "tournaments-c1329",
+  storageBucket: "tournaments-c1329.appspot.com",
+  messagingSenderId: "764943226009",
+  appId: "1:764943226009:web:e2714326db281661611408",
+  measurementId: "G-F55ZCJ8SFH",
+};
---
+++ screens/LoginScreen.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const singIn = () => {};
+
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{ uri: "http://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png" }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autofocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button containerStyle={styles.button} onPress={singIn} title="Login" />
+      <Button containerStyle={styles.button} type="outline" title="Register" />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {},
+  button: {},
+});
